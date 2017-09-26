0b37f313dec07be9b87fd6b0c043a26c5f74dfca
gzygzy9211 <gzygzy9211@gmail.com>
2017-09-26 11:10:28 +0800

    finish convert_dense_net implementation

--- tools/convert_dense_net.cpp
+++ tools/convert_dense_net.cpp
@@ -1,25 +1,34 @@
+#include "caffe/common.hpp"
 #include "caffe/proto/caffe.pb.h"
 #include "caffe/util/upgrade_proto.hpp"
 #include "caffe/util/io.hpp"
-#include "caffe/common.hpp"
 #include "caffe/net.hpp"
 #include "caffe/layers/dense_block_layer.hpp"
 #include <cstdio>
+#include <map>
 
 using namespace caffe;
 
 typedef float Dtype;
 
+static Layer<Dtype>* get_layer(const Net<Dtype> &net, const std::string &name)
+{
+  Layer<Dtype>* ret = NULL;
+  ret = net.layer_by_name(name).get();
+  CHECK(ret) << "Layer " << name << " does not exists";
+  return ret;
+}
+
 int main(int argc, char **argv)
 {
-  if (argc != 4)
+  if (argc != 5)
   {
     LOG(ERROR) << "Usage: " << argv[0] << " {input.prototxt} {input.caffemodel} {output.prototxt} {output.caffemodel}";
     return argc;
   }
   
 
-  NetParameter input_weigths;
+  NetParameter input_weights;
   ReadNetParamsFromBinaryFileOrDie(argv[2], &input_weights);
   NetParameter input_net;
   ReadNetParamsFromTextFileOrDie(argv[1], &input_net);
@@ -32,8 +41,9 @@ int main(int argc, char **argv)
   
   {
     Net<Dtype> &net = *(the_net.get());
-    net.CopyTrainedLayersFrom(input_weigths);
-    
+    net.CopyTrainedLayersFrom(input_weights);
+    std::map<std::string, std::vector<std::string> > names_layer_dense;
+     
     { // construct output prototxt
       output_net.CopyFrom(input_net);
       output_net.clear_layer();
@@ -46,14 +56,19 @@ int main(int argc, char **argv)
             (DenseBlockLayer<Dtype>*)(net.layer_by_name(layer_name).get());
           std::vector<LayerParameter> dense_layers;
           dense_block->convertToPlainLayers(dense_layers);
+          names_layer_dense[input_net.layer(layer_id).name()] = std::vector<std::string>();
+          std::vector<std::string> &names = names_layer_dense[input_net.layer(layer_id).name()];
           for (size_t i = 0; i < dense_layers.size(); i++)
+          {
             output_net.add_layer()->CopyFrom(dense_layers[i]);
+            names.push_back(dense_layers[i].name());
+          }
         }
         else
           output_net.add_layer()->CopyFrom(input_net.layer(layer_id));
       }
       FILE *fp = fopen(argv[3], "wb");
-      CHECK_NE(fp, NULL) << "fail to open file " << argv[3];
+      CHECK(fp) << "fail to open file " << argv[3];
       fprintf(fp, "%s\n", output_net.DebugString().c_str());
       fclose(fp);
     }
@@ -65,16 +80,20 @@ int main(int argc, char **argv)
     Net<Dtype> &out_net = *(the_out_net.get());
 
     { // copy weights to output net
-      int dst_id = 0;
-      for (int src_id = 0; src_id < net.layers().size(); src++)
+      for (int src_id = 0; src_id < net.layers().size(); src_id++)
       {
         Layer<Dtype>* src_layer = net.layers()[src_id].get();
         if (src_layer->layer_param().type() == "DenseBlock")
         {
           int src_offset = 0;
-          while (src_offset < src_layer->blobs().size())
+          CHECK(names_layer_dense.find(src_layer->layer_param().name()) != names_layer_dense.end())
+            << "DenseBlockLayer " << src_layer->layer_param().name() << "does not exist";
+          const std::vector<std::string> &names_dst_layers = names_layer_dense[src_layer->layer_param().name()];
+          //while (src_offset < src_layer->blobs().size())
+          for (size_t dense_i = 0; dense_i < names_dst_layers.size(); dense_i ++)
           {
-            Layer<Dtype>* dst_layer = out_net.layers()[dst_id].get();
+            const std::string &dst_name(names_dst_layers[dense_i]);
+            Layer<Dtype>* dst_layer = get_layer(out_net, dst_name);
             CHECK_LE(dst_layer->blobs().size() + src_offset, src_layer->blobs().size())
               << "#blobs dismatch within DenseBlock " << src_layer->layer_param().name();
 
@@ -82,33 +101,33 @@ int main(int argc, char **argv)
             {
               CHECK(src_layer->blobs()[blob_id + src_offset]->shape() 
                  == dst_layer->blobs()[blob_id]->shape()) 
-                << "blob shape dismatch at src(" << src_id << ") vs dst(" << dst_id ")"
+                << "blob shape dismatch at src(" << (src_offset + blob_id) << ") vs dst(" << dst_name << ")"
                 << " blob[" << blob_id << "]";
               caffe_copy(src_layer->blobs()[blob_id + src_offset]->count(),
                          src_layer->blobs()[blob_id + src_offset]->cpu_data(),
                          dst_layer->blobs()[blob_id]->mutable_cpu_data());
             }
-
+            LOG(INFO) <<  dst_layer->blobs().size() << " " << dst_layer->layer_param().name();
             src_offset += dst_layer->blobs().size();
-            dst_id++;
           }
+          CHECK_EQ(src_offset, (int)(src_layer->blobs().size())) 
+            << "DenseBlockLayer is not converted completely";
         }
         else
         {
-          Layer<Dtype>* dst_layer = out_net.layers()[dst_id].get();
+          Layer<Dtype>* dst_layer = get_layer(out_net, src_layer->layer_param().name());
           CHECK_EQ(src_layer->blobs().size(), dst_layer->blobs().size())
-            << "#blobs dismatch at src(" << src_id << ") vs dst(" << dst_id ")";
+            << "#blobs dismatch at " << src_layer->layer_param().name();
           for (int blob_id = 0; blob_id < src_layer->blobs().size(); blob_id++)
           {
             CHECK(src_layer->blobs()[blob_id]->shape() 
                == dst_layer->blobs()[blob_id]->shape()) 
-              << "blob shape dismatch at src(" << src_id << ") vs dst(" << dst_id ")"
+              << "blob shape dismatch at " << src_layer->layer_param().name()
               << " blob[" << blob_id << "]";
             caffe_copy(src_layer->blobs()[blob_id]->count(),
                        src_layer->blobs()[blob_id]->cpu_data(),
                        dst_layer->blobs()[blob_id]->mutable_cpu_data());
           }
-          dst_id ++;
         }
       }
     }
